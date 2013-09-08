119524f9c0a9f319a7034eae1564078cc99e1701
JeanFred <JeanFred@github>
2013-09-08 22:32:19 +0200

    Add setup script
    As a user, I want to install the dependencies without any problem.
    The setup scripts (which has other purposes) allow this in
    particular, by using the install_requires directive together
    with the dependency_links one. This allows us to retrieve the
    MassUploadLibrary from GitHub seamlessly.

---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+packages = []
+requires = ['MassUploadLibrary']
+dependency_links = ['https://github.com/JeanFred/MassUploadLibrary/archive/master.tar.gz#egg=MassUploadLibrary'],
+scripts  = []
+
+setup(
+    name         = 'ArchivesNationales',
+    version      = '0.1',
+    description  = 'Managing the Archives Nationales metadata conversion.',
+    author       = 'Jean-Frederic',
+    author_email = 'JeanFred@github',
+    url          = 'http://github.com.org/JeanFred/ArchivesNationales',
+    packages         = packages,
+    install_requires = requires,
+    dependency_links = dependency_links,
+    )
