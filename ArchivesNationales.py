f9e5fb599ee76c40beaea2957a2937d707633a4d
Jean-Fred <JeanFred@github>
2015-04-26 23:46:49 +0200

    Update with latest additions

--- ArchivesNationales.py
+++ ArchivesNationales.py
@@ -10,6 +10,7 @@ import re
 from uploadlibrary import metadata
 from uploadlibrary.UploadBot import DataIngestionBot, UploadBotArgumentParser
 from processors import split_and_apply_template_on_each, look_for_sizes
+import uploadlibrary.PostProcessing as commonprocessors
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
@@ -22,16 +23,15 @@ class ArchivesMetadataCollection(metadata.MetadataCollection):
     def handle_record(self, image_metadata):
         """Handle a record."""
         filename = image_metadata['Fichier']
-        path = os.path.abspath(os.path.join('.', 'WIKI', filename))
-        image_metadata['subst'] = 'subst:'
+        path = os.path.abspath(os.path.join('.', 'third', filename))
         return metadata.MetadataRecord(path, image_metadata)
 
 
 def main(args):
     """Main method."""
     collection = ArchivesMetadataCollection()
-    csv_file = 'Metadata_ArchivesNationales.csv'
-    collection.retrieve_metadata_from_csv(csv_file, delimiter=',')
+    csv_file = 'Metadata_ArchivesNationales3-Cut.csv'
+    collection.retrieve_metadata_from_csv(csv_file, delimiter=';')
 
     alignment_template = 'User:Jean-Frédéric/AlignmentRow'.encode('utf-8')
 
@@ -44,32 +44,39 @@ def main(args):
         mapping_fields = []
         mapper = commonprocessors.retrieve_metadata_alignments(mapping_fields,
                                                                alignment_template)
-        support_mapper = {
-            'papier': 'paper',
-            'parchemin': 'parchment',
-            'cire': 'wax',
-            'cuir': 'leather',
-            'plâtre': 'plaster',
-            'bois': 'wood',
-            'érable': 'maple',
-            'velin': 'Vellum'
-        }
         mapping_methods = {
-        'Support': (split_and_apply_template_on_each, {'template': 'Technique', 'mapper': support_mapper}),
-        'Dimensions du document': (look_for_sizes, {})
+        'Support': commonprocessors.map_and_apply_technique(),
+        'Dimensions du document': (commonprocessors.parse_format, {}),
+        'Date du document': (commonprocessors.look_for_date, {}),
+        'Description du document': (commonprocessors.remove_linebreaks, {}),
+        'Cote du document': (commonprocessors.remove_linebreaks, {}),
+        'Titre du document': (commonprocessors.remove_linebreaks, {})
         }
 
-        
-        reader = collection.post_process_collection(mapping_methods)
-        template_name = 'subst:Commons:Archives_Nationales/Ingestion'.encode('utf-8')
-        front_titlefmt = ""
-        variable_titlefmt = "%(Titre du document)s %(Page)s"
-        rear_titlefmt = " - Archives Nationales - %(Cote du document)s.%(_ext)s"
-        uploadBot = DataIngestionBot(reader=reader,
-                                     front_titlefmt=front_titlefmt,
-                                     rear_titlefmt=rear_titlefmt,
-                                     variable_titlefmt=variable_titlefmt,
-                                     pagefmt=template_name)
+        categories_counter, categories_count_per_file = collection.post_process_collection(mapping_methods)
+        #metadata.categorisation_statistics(categories_counter, categories_count_per_file)
+
+
+    template_name = 'Commons:Archives_Nationales/Ingestion'.encode('utf-8')
+    front_titlefmt = ""
+    variable_titlefmt = "%(Titre du document)s %(Page)s"
+    rear_titlefmt = " - Archives Nationales - %(Cote du document)s"
+    reader = iter(collection.records)
+    uploadBot = DataIngestionBot(reader=reader,
+                                 front_titlefmt=front_titlefmt,
+                                 rear_titlefmt=rear_titlefmt,
+                                 variable_titlefmt=variable_titlefmt,
+                                 pagefmt=template_name)
+
+    reader = iter(collection.records)
+    uploadBot = DataIngestionBot(reader=iter(reader),
+                                 front_titlefmt=front_titlefmt,
+                                 rear_titlefmt=rear_titlefmt,
+                                 variable_titlefmt=variable_titlefmt,
+                                 pagefmt=template_name,
+                                 subst=True,
+                                 verifyDescription=False
+                                 )
 
     if args.upload:
         uploadBot.run()
